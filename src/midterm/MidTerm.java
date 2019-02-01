6c0a7d01a370352c7132fe6e4df0f4351cb71277
David Wolverton <dwolvert@gmail.com>
2019-02-01 13:40:09 -0500

    obviously

--- src/midterm/MidTerm.java
+++ src/midterm/MidTerm.java
@@ -6,5 +6,7 @@ public class MidTerm {
 		// hello conflict!!
 		System.out.println("Welcome.");
 		System.out.println("Mariah is DEF the best.");
+		System.out.println("David is more awesome than sliced bread!");
+
 	}
 }
