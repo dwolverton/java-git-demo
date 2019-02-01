cca355a1c425cee5b0f6077107e5bf28ea2e3c0a
Mariah Hall <hallmar94@gmail.com>
2019-02-01 13:35:54 -0500

    ..

--- src/midterm/MidTerm.java
+++ src/midterm/MidTerm.java
@@ -6,5 +6,6 @@ public class MidTerm {
 		// hello conflict!!
 		System.out.println("Welcome.");
 		System.out.println("Mariah is DEF the best.");
+		System.out.println("Java is the best, I guess.");
 	}
 }
